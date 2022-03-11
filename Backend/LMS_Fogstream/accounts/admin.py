2f37841cc9678250aae00af387da4237f0ad2815
dolphin <dosometh@mail.com>
2022-03-11 17:38:26 +1000

    added filters and fieldsets

--- Backend/LMS_Fogstream/accounts/admin.py
+++ Backend/LMS_Fogstream/accounts/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import UserProfile
 
 
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "group", "date_joined", "updated_on", "is_creator")
+    list_filter = ("group", "date_joined", "updated_on", "is_creator")
--- Backend/LMS_Fogstream/lms/admin.py
+++ Backend/LMS_Fogstream/lms/admin.py
@@ -2,9 +2,37 @@ from django.contrib import admin
 from .models import Category, Course, Group, Review, Lessons, LessonCategory
 
 
-admin.site.register(Category)
-admin.site.register(Course)
-admin.site.register(Group)
-admin.site.register(Review)
-admin.site.register(Lessons)
-admin.site.register(LessonCategory)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "descriptions")
+    list_filter = ("name",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "tagline", "draft")
+    list_filter = ("category", "draft")
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "course", "date")
+    list_filter = ("course", "date")
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("course", "email", "text")
+    list_filter = ("course", "email", "text")
+
+
+@admin.register(Lessons)
+class LessonsAdmin(admin.ModelAdmin):
+    list_display = ("title", "lesson_category", "type", "completed", "score", "draft")
+    list_filter = ("lesson_category", "type", "score", "completed", "draft")
+
+
+@admin.register(LessonCategory)
+class LessonCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "course")
+    list_filter = ("course",)
